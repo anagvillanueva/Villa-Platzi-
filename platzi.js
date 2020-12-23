33060e82dd2ab3bd004cb89068eb7307463894a0
anagvillanueva <anafernandagv27@gmail.com>
2020-12-23 11:58:39 -0600

    Subiendo primeros archivos

---
+++ platzi.js
@@ -0,0 +1,19 @@
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+var mapa = "tile.png";
+
+var imagen = new Image();
+imagen.src = mapa;
+imagen.addEventListener("load", dibujar);
+
+function dibujar()
+{
+  papel.drawImage(imagen, 0, 0);  
+}
+
+function aleatorio(min, maxi)
+{
+  var resultado;
+  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+  return resultado;
+}
